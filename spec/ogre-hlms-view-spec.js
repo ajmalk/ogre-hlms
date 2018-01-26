4d0f67e3c6cd4e83f7ba7353f4fcff8e5759126a
Ajmal Kunnummal <ajmal.knml@gmail.com>
2018-01-26 12:22:45 -0500

    init

---
+++ spec/ogre-hlms-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OgreHlmsView from '../lib/ogre-hlms-view';
+
+describe('OgreHlmsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
